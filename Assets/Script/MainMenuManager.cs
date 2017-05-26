0e7d772a44ab2738cffdb1f213ed6cc4f741618d
Julia <julia141407@gmail.com>
2017-05-26 13:00:37 +0300

    first commit

---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject Pause_panel = null;
+    public GameObject SubPanel = null;
+	public bool _PanelForAboutOpened = false;
+
+    public void Settings()
+	{
+		
+	}
+
+	public void GamePlay()
+	{
+		SceneManager.LoadScene ("Game Scena");
+	}
+
+	public void ToggleSubPanelOpen()
+	{
+        if (!GameManagerinstance.IsPaused)
+        {
+            _PanelForAboutOpened = !_PanelForAboutOpened;
+        }
+        SetSubPanelVisibility();
+    }
+	private void SetSubPanelVisibility()
+    {
+        if (SubPanel != null)
+        {
+            SubPanel.SetActive(_PanelForAboutOpened);
+        }
+    }
+   private void CloseSubeMenu()
+    {
+      _PanelForAboutOpened = false;
+        SubPanel.SetActive(_PanelForAboutOpened);
+    }
+
+    private class GameManagerinstance
+    {
+        public static bool IsPaused { get; internal set; }
+    }
+    public void CLOSE()
+    {
+        _PanelForAboutOpened = false;
+        SubPanel.SetActive(_PanelForAboutOpened);
+    }
+    
+    }
+
